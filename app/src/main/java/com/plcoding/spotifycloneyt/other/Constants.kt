2b9130bb9591122861b71205a53e59c86efb3258
abhisatria <45398198+abhisatria@users.noreply.github.com>
2023-04-10 02:15:31 +0700

    setup music notification

--- app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/other/Constants.kt
@@ -1,5 +1,8 @@
 package com.plcoding.spotifycloneyt.other
 
 object Constants {
-    const val SONG_COLLECTION = "songs";
+    const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
