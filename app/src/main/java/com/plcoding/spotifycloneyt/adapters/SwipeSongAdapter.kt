4d25a80207aa1d87e6b19aa62d91ab874483748a
abhisatria <45398198+abhisatria@users.noreply.github.com>
2023-04-28 01:36:06 +0700

    init song adapter

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/adapters/SwipeSongAdapter.kt
@@ -0,0 +1,26 @@
+package com.plcoding.spotifycloneyt.adapters
+
+import androidx.recyclerview.widget.AsyncListDiffer
+import com.plcoding.spotifycloneyt.R
+import kotlinx.android.synthetic.main.list_item.view.*
+import javax.inject.Inject
+
+class SwipeSongAdapter @Inject constructor(
+) : BaseSongAdapter(R.layout.list_item) {
+
+    override val differ = AsyncListDiffer(this, diffCallback)
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songs[position]
+        holder.itemView.apply {
+            val text = "${song.title} - ${song.subtitle}"
+            tvPrimary.text = text
+
+            setOnClickListener {
+                onItemClickListener?.let { click ->
+                    click(song)
+                }
+            }
+        }
+    }
+}
